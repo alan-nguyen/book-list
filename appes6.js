a9ae9d304c9b226406f85f3007d2782fb5e5197c
Alan Nguyen <tuan8101@gmail.com>
2020-02-23 22:23:16 -0700

    Create cass UI and addBookToList method

--- appes6.js
+++ appes6.js
@@ -5,3 +5,26 @@ class Book {
     this.isbn = isbn;
   }
 }
+
+class UI {
+  addBookToList(book) {
+    const list = document.getElementById("book-list");
+    // Create tr element
+    const row = document.createElement("tr");
+    // Insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+
+    list.appendChild(row);
+  }
+
+  showAlert(message, className) {}
+
+  deleteBook(target) {}
+
+  clearField() {}
+}
