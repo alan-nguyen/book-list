d40cf490300a86d03eee382058a437c32c848027
Alan Nguyen <tuan8101@gmail.com>
2020-02-23 22:24:37 -0700

    Create showAlert method

--- appes6.js
+++ appes6.js
@@ -22,7 +22,25 @@ class UI {
     list.appendChild(row);
   }
 
-  showAlert(message, className) {}
+  showAlert(message, className) {
+    // Create div
+    const div = document.createElement("div");
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector(".container");
+    // Get form
+    const form = document.querySelector("#book-form");
+    // Insert alert
+    container.insertBefore(div, form);
+
+    // Timeout after 3 sec
+    setTimeout(function() {
+      document.querySelector(".alert").remove();
+    }, 3000);
+  }
 
   deleteBook(target) {}
 
