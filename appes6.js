5362522a258d49eb5764978c52ab75d6caab9438
Alan Nguyen <tuan8101@gmail.com>
2020-02-23 22:32:33 -0700

    Create clearField method

--- appes6.js
+++ appes6.js
@@ -48,5 +48,54 @@ class UI {
     }
   }
 
-  clearField() {}
+  clearFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+  }
 }
+
+// Event Listeners for add book
+document.getElementById("book-form").addEventListener("submit", function(e) {
+  // Get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    // Error alert
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Show success
+    ui.showAlert(`Book added!`, "success");
+
+    // Clear fields
+    ui.clearFields();
+  }
+
+  e.preventDefault();
+});
+
+// Event Listener for delete
+document.getElementById("book-list").addEventListener("click", function(e) {
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert(`Book deleted!`, "success");
+
+  e.preventDefault();
+});
